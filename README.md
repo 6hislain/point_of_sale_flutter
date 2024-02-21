171c87488fbbd6756b18826679ff24e54881fff8
6hislain <ghislainmike80@gmail.com>
2024-02-21 22:37:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# point_of_sale_flutter
+
+A new Flutter project.
+
+## TODO
+
+- [ ] category
+- [ ] product
+- [ ] client
+- [ ] transaction
---
+++ android/app/src/main/kotlin/com/example/point_of_sale_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.point_of_sale_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
